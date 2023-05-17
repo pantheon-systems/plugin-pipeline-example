bca76de8329b453d1454092d5d969283f6652afc
Phil Tyler <phil.tyler@pantheon.io>
2023-05-17 10:33:47 -0700

    Scaffold Plugin and setup tests and automation (#1)

---
+++ rossums-universal-robots.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Rossums Universal Robots
+ * Plugin URI:      pantheon.io
+ * Description:     A demo plugin to test and showcase github actions during plugin development and release
+ * Author:          Pantheon Systems Inc
+ * Author URI:      pantheon.io
+ * Text Domain:     rossums-universal-robots
+ * Domain Path:     /languages
+ * Version:         0.1.0-dev
+ *
+ * @package         Rossums_Universal_Robots
+ */
+
+// Your code starts here.
