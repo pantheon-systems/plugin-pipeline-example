b65ca6d2219296066ca745e4364fdd10feb4b897
CobyPear <coby.sher@pantheon.io>
2023-12-18 17:23:39 -0600

    Add release step to build-tag.yml and get_release_notes.js
    - get_release_notes.js takes a file path and scrapes the text between releases
    - use gh cli to generate release based on the tag

---
+++ scripts/get_release_notes.js
@@ -0,0 +1,27 @@
+"use strict";
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+
+// get the args
+const [path] = process.argv.slice(2);
+// throw if necessary args are missing
+if (!path) {
+  throw new Error("Please provide a path to the file to parse");
+}
+const filePath = resolve(__dirname,'..', path);
+const fileContent = readFileSync(filePath, "utf8");
+/**
+ * The following input:
+ * 
+ * ### 0.1.3-dev
+ * * Set Counter to 1 [20](https://github.com/pantheon-systems/plugin-pipeline-example/pull/20)
+ * ### 0.1.2-dev (18 December 2023)
+ * * Set Counter to 0 [19](https://github.com/pantheon-systems/plugin-pipeline-example/pull/19)
+ * 
+ * would have the following match:
+ * * Set Counter to 1 [20](https://github.com/pantheon-systems/plugin-pipeline-example/pull/20)
+ */
+const regex = /(^#{3}\s[\s\d\.]+(\([\w\d\s]+\))?$\n(?<notes>^[\w\d\W][^#]+$\n))/gm
+const matches = fileContent.matchAll(regex);
+const [releaseNotes] = [...matches].map(match => match.groups.notes.trim());
+console.log(releaseNotes);
