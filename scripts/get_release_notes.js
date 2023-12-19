a82667bf5be159f85d26872d14c07f7237dd1466
CobyPear <coby.sher@pantheon.io>
2023-12-19 11:27:27 -0600

    Formatting

--- scripts/get_release_notes.js
+++ scripts/get_release_notes.js
@@ -8,20 +8,21 @@ const [path] = process.argv.slice(2);
 if (!path) {
   throw new Error("Please provide a path to the file to parse");
 }
-const filePath = resolve(__dirname,'..', path);
+const filePath = resolve(__dirname, "..", path);
 const fileContent = readFileSync(filePath, "utf8");
 /**
  * The following input:
- * 
+ *
  * ### 0.1.3-dev
  * * Set Counter to 1 [20](https://github.com/pantheon-systems/plugin-pipeline-example/pull/20)
  * ### 0.1.2-dev (18 December 2023)
  * * Set Counter to 0 [19](https://github.com/pantheon-systems/plugin-pipeline-example/pull/19)
- * 
+ *
  * would have the following match:
  * * Set Counter to 1 [20](https://github.com/pantheon-systems/plugin-pipeline-example/pull/20)
  */
-const regex = /(^#{3}\s[\s\d\.]+(\([\w\d\s]+\))?$\n(?<notes>^[\w\d\W][^#]{3,}$\n))/gm
+const regex =
+  /(^#{3}\s[\s\d\.]+(\([\w\d\s]+\))?$\n(?<notes>^[\w\d\W][^#]{3,}$\n))/gm;
 const matches = fileContent.matchAll(regex);
-const [releaseNotes] = [...matches].map(match => match.groups.notes.trim());
+const [releaseNotes] = [...matches].map((match) => match.groups.notes.trim());
 console.log(releaseNotes);
