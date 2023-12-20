ea99b045da737811aceef50d55c0d72b34a0990d
Phil Tyler <phil.tyler@pantheon.io>
2023-12-19 16:17:08 -0800

    sed specific to readmes

--- .bin/prepare-dev.sh
+++ .bin/prepare-dev.sh
@@ -38,10 +38,19 @@ process_file(){
         echo "package and package-lock will be handled later."
         return
     fi
-    if [[ "$file" == "$BASE_DIR/composer.json" || "$file" == "$BASE_DIR/composer-lock.json" ]];then
+    if [[ "$file" == "$BASE_DIR/composer.json" || "$file" == "$BASE_DIR/composer.lock" ]];then
         echo "skip composer."
         return
     fi
+
+    shopt -s nocasematch # make the "if readme" case insensitive
+    for readme_extension in "txt" "md"; do
+        if [[ "$file" == "${BASE_DIR}/readme.${readme_extension}" ]]; then
+            echo "skip readmes"
+            continue
+        fi
+    done
+    shopt -u nocasematch
 
     echo "search-and-replace with sed"
     sed -i.tmp -e '/^\s*\* @since/!s/'"${CANONICAL_VERSION}"'/'"${NEW_DEV_VERSION}"'/g' "$file" && rm "$file.tmp"
@@ -74,6 +83,8 @@ update_readme(){
     fi
     awk -v heading="$new_heading" "$awk_with_target" "$FILE_PATH" > tmp.md
     mv tmp.md "$file"
+
+    sed -i.tmp -e "s/Tested up to: ${CANONICAL_VERSION}/Tested up to: ${NEW_DEV_VERSION}/g" "$file" && rm "$file.tmp"
 
     git add "$file"
 }
